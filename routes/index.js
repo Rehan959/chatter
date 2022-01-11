78ecd5bd360e938689f23f8ea3922cc7cfb922cd
Rehan959 <rehanshami007@gmail.com>
2022-01-11 20:05:07 +0530

    clock

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.post('/chat',(req,res)=>{
+  let name = req.body.name;
+  console.log(name)
+  res.render('chat', {name})
+})
 
 module.exports = router;
